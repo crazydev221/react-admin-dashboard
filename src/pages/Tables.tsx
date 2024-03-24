06fbb514e02a19fb04681e0caa98dc36cf62ad3a
Full-StackProDev <crazydev221@gmail.com>
2024-03-24 08:31:02 -0500

    react admin dashboard with ts and tailwind

---
+++ src/pages/Tables.tsx
@@ -0,0 +1,21 @@
+import Breadcrumb from '../components/Breadcrumbs/Breadcrumb';
+import TableOne from '../components/Tables/TableOne';
+import TableThree from '../components/Tables/TableThree';
+import TableTwo from '../components/Tables/TableTwo';
+import DefaultLayout from '../layout/DefaultLayout';
+
+const Tables = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="Tables" />
+
+      <div className="flex flex-col gap-10">
+        <TableOne />
+        <TableTwo />
+        <TableThree />
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default Tables;
